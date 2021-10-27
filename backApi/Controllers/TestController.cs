9f84125a6274eb5302987b50056baf93a4c0e721
eloyEntony <kozyar.ant@gmail.com>
2021-10-27 11:10:40 +0300

    first commit

---
+++ backApi/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+       [HttpGet]
+        public string Test()
+        {
+            return "succeed";
+        }
+    }
+}
